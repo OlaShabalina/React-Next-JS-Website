b8802ef3736b3f28ac8c783992a9d4cf41110d5a
OlaShabalina <o.shabalina@outlook.com>
2021-12-05 16:25:24 +1100

    added media queries and humburger bar to the styles components of nav bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Nav = styled.nav`
   height: 80px;
@@ -18,15 +19,54 @@ const Logo = styled.a`
   color: #154c79;
 `
 
+const FullNav = styled.div`
+  display: flex;
+
+  @media (max-width: 850px) {
+    flex-direction: column;
+    position: absolute;
+    top: 80px;
+    right: 0;
+    background-color: white;
+    justify-content: center;
+    align-items: center;
+    width: 300px;
+    float: right;
+  }
+`
+
 const StyledLink = styled.a`
   padding: 0 2em;
 
   &:hover {
     color: #154c79;
   }
+
+  @media (max-width: 850px) {
+    padding: 10px 20px;
+  }
+`
+
+const HanburgerMenu = styled.button`
+  background-color: transparent;
+  border: 0;
+  font-size: 20px;
+  color: #154c79;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media (min-width: 850px) {
+    display: none;
+  }
 `
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <Nav>
       <div>
@@ -34,7 +74,10 @@ const Navbar = () => {
           <Logo>AroundMelbourne</Logo>
         </Link>
       </div>
-      <div>
+      <HanburgerMenu onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"}> </i>
+      </HanburgerMenu>
+      <FullNav>
         <Link href="/" passHref>
           <StyledLink>Home</StyledLink>
         </Link>
@@ -47,7 +90,7 @@ const Navbar = () => {
         <Link href="/contact" passHref>
           <StyledLink>Get in touch</StyledLink>
         </Link>
-      </div>
+      </FullNav>
     </Nav>
   )
 }
